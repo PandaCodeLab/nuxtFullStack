f2c8260aa0fc48a0a36e7aee83d862bf6d2661bc
StarBurst <panda.code.lab@gmail.com>
2021-03-24 17:05:22 +0300

    3_6-done

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,15 +1,59 @@
 <template>
-  <article class="post">321</article>
+  <el-card shadow="hover" :body-style="{ padding: 0 }" class="post">
+    <header slot="header" class="post__header">
+      <h3>Post Title</h3>
+
+      <small
+        ><i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post__body">
+      <img
+        src="https://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL-640x400.jpg"
+        alt=""
+        class="post__img"
+      />
+    </div>
+    <footer class="post__footer">
+      <el-button round type="primary" @click="openPost">Primary</el-button>
+
+      <span>
+        <i class="el-icon-message">12</i>
+      </span>
+    </footer>
+  </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    openPost() {
+      const id = '1'
+      this.$router.push(`/post/${id}`)
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 .post {
-  height: 200px;
-  border: 1px solid black;
-  margin-bottom: 2rem;
+  margin-bottom: 1.5rem;
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &__footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+  }
+
+  &__img {
+    width: 100%;
+    height: auto;
+  }
 }
 </style>
---
+++ pages/post/_id.vue
@@ -0,0 +1,87 @@
+<template>
+  <article class="post">
+    <header class="post__header">
+      <div class="post__title">
+        <h1>Post title</h1>
+        <nuxt-link to="/">
+          <i class="el-icon-back"></i>
+        </nuxt-link>
+      </div>
+      <div class="post__info">
+        <small
+          ><i class="el-icon-time"></i>
+          {{ new Date().toLocaleString() }}
+        </small>
+        <small></small>
+      </div>
+      <div class="post__img">
+        <img
+          src="https://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL-640x400.jpg"
+          alt=""
+        />
+      </div>
+    </header>
+    <main class="post__content">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sequi
+        ipsa voluptas odit laudantium temporibus repellat quas incidunt tempore!
+        Sequi quos assumenda deserunt debitis repellat soluta sunt aliquid dolor
+        consectetur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sequi
+        ipsa voluptas odit laudantium temporibus repellat quas incidunt tempore!
+        Sequi quos assumenda deserunt debitis repellat soluta sunt aliquid dolor
+        consectetur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sequi
+        ipsa voluptas odit laudantium temporibus repellat quas incidunt tempore!
+        Sequi quos assumenda deserunt debitis repellat soluta sunt aliquid dolor
+        consectetur.
+      </p>
+    </main>
+  </article>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    return !!params.id
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.post {
+  max-width: 600px;
+  margin: 0 auto;
+
+  &__title {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+    align-items: center;
+  }
+
+  &__info {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.5rem;
+    align-items: center;
+  }
+
+  &__img img {
+    width: 100%;
+    height: auto;
+  }
+
+  &__header {
+    margin-bottom: 1.5rem;
+  }
+
+  &__content {
+    margin-bottom: 2rem;
+  }
+}
+</style>
