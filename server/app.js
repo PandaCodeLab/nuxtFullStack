2921580f69f197ea7da492bd34ec0346bda3ba06
StarBurst <panda.code.lab@gmail.com>
2021-03-30 20:16:41 +0300

    JWT done

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const passportStrategy = require('./middleware/passport-strategy')
 const authRouters = require('./routes/auth.routes')
 
 const keys = require('./keys')
@@ -10,6 +12,9 @@ mongoose
   .connect(keys.MONGO_URI)
   .then(() => console.log('MongoDB Connected!'))
   .catch(e => console.error(e))
+
+app.use(passport.initialize())
+passport.use(passportStrategy)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
