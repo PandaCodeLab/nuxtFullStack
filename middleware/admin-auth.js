8ac453ceadfb57e24c980134983d5592dad8bfcd
StarBurst <panda.code.lab@gmail.com>
2021-03-30 03:12:56 +0300

    server/keys .gitignore

--- middleware/admin-auth.js
+++
@@ -1,5 +0,0 @@
-export default function({ store, redirect }) {
-  if (!store.getters['auth/isLogged']) {
-    redirect('/admin/login?message=login')
-  }
-}
--- server/keys/keys.prod.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  MONGO_URI: `mongodb+srv://d3gfaw:08ebdc0ced@cluster0.erzim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-}
